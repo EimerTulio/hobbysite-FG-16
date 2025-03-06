b75ae9acc460aa913d08666ba90a570411c4512a
youth-of-may <princess.giron@obf.ateneo.edu>
2025-03-07 06:56:48 +0800

    Added list and detail views to views.py.

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Post, PostCategory
 
-# Create your views here.
+
+def index_view(request):
+    return render(request, 'base.html')
+
+
+def list_view(request):
+    categories = PostCategory.objects.all()
+    context = {
+        "categories": categories,
+    }
+    return render(request, 'forum/threads.html', context)
+
+
+def detail_view(request, pk):
+    category = PostCategory.objects.get(pk=pk)
+    context = {
+        "category": category,
+    }
+    return render(request, 'forum/thread/1.html', context)
