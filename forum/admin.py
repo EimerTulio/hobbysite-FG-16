2ce79109d4b5771ee91fc9f852d24a4c604d4f74
youth-of-may <princess.giron@obf.ateneo.edu>
2025-03-07 07:01:18 +0800

    Set-up admin.py to create PostCategoryAdmin and PostAdmin.

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import PostCategory, Post
 
-# Register your models here.
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+    search_fields = ('name',)
+    list_display = ('name',)
+    fieldsets = [
+        (
+            'Post Category Information',
+            {'fields': ['name', 'description']},
+        ),
+    ]
+
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+    search_fields = ('title',)
+    list_display = ('title', 'time_created')
+    fieldsets = [
+        (
+            'Posts Information',
+            {'fields': ['title', 'entry', 'category']},
+        ),
+    ]
+
+
+admin.site.register(PostCategory, PostCategoryAdmin)
+admin.site.register(Post, PostAdmin)
