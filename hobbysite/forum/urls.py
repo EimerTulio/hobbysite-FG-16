4455b363d1958d15add5e725199f8787431f701d
EimerTulio <eimer.tulio@student.ateneo.edu>
2025-03-07 23:55:11 +0800

    blog fix

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -4,7 +4,7 @@ from .views import index_view, list_view, detail_view
 urlpatterns = [
     path('', index_view, name='index'),
     path('threads', list_view, name='post-category'),
-    path('thread/<int:pk>/', detail_view, name='post'),
+    path('thread/<int:pk>', detail_view, name='post'),
 ]
 
 app_name = 'forum'
