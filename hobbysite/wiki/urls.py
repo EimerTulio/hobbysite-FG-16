3af994bc836b01d61575876a98e2db4d6fe669c7
Alba Martin <martin.alba@student.ateneo.edu>
2025-05-05 15:25:48 +0800

    Fixed article/detail and article/list to article-detail and article-list respectively.

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import article_list, article_detail
 
 urlpatterns = [
-path('articles', article_list, name="article/list"),
-path('article/<int:pk>', article_detail, name="article/detail"),
+path('articles', article_list, name="article-list"),
+path('article/<int:pk>', article_detail, name="article-detail"),
 ]
 # This might be needed, depending on your Django version
 app_name = "wiki"
