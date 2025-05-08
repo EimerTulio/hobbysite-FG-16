eee70938b4070e48eaa44d6bf0b489a7188824ad
Alba Martin <martin.alba@student.ateneo.edu>
2025-05-08 13:42:49 +0800

    Fixed errors and issue with homepage redirection. Now properly redirects to the base localhost correctly.

--- hobbysite/homepage/urls.py
+++ hobbysite/homepage/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index_view
 
 urlpatterns = [
-    path('home/', index_view, name='index'),
+    path('', index_view, name='index'),
 ]
 
 app_name = 'homepage'
