4790edad8c3bbb9f1b279506fdf5f5cf402cde7c
Alba Martin <martin.alba@student.ateneo.edu>
2025-05-12 03:10:41 +0800

    Major bug fixes and added features to wiki.
    Added the ability for users to add articles to the wiki. Also added the ability have pictures in the articles for wiki

--- hobbysite/wiki/models.py
+++ hobbysite/wiki/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 class ArticleCategory(models.Model):
     '''ArticleCategory model for wiki articles'''
@@ -9,28 +10,59 @@ class ArticleCategory(models.Model):
         return '{}: ArticleCategory'.format(self.name)
     
     def get_absolute_url(self):
-        return reverse('wiki:article-detail', args=[self.pk])
+        return reverse('wiki:article-list')
     
     class Meta:
         ordering = ['name']
-    
+        verbose_name = 'Article Category'
+        verbose_name_plural = 'Article Categories'
+
 class Article(models.Model):
     '''Article model for wiki articles'''
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        related_name="articles",
+        null=True,
+    )
     category = models.ForeignKey(
         ArticleCategory,
         on_delete=models.SET_NULL,
         related_name="articles",
-        null=True
+        null=True,
     )
     entry = models.TextField()
+    header_image = models.ImageField(upload_to='article_headers/', null=True, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return '{}: Article'.format(self.title)
     
+    def get_absolute_url(self):
+        return reverse('wiki:article-detail', args=[self.pk])
+    
     class Meta:
         ordering = ['-created_on']
+        
+class Comment(models.Model):
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.SET_NULL,
+        related_name="comments",
+        null=True,
+    )
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return '{}: Comment'.format(self.title)
     
-    
+    class Meta:
+        ordering = ['-created_on']
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import ArticleCategory, Article
+from .forms import ArticleForm, CommentForm
 
     
 def article_list(request):
@@ -17,3 +18,34 @@ def article_detail(request, pk):
         "article" : article
     }
     return render(request, 'wiki_detail.html', ctx)
+
+def article_add(request):
+    '''Article create view for wiki articles'''
+    form = ArticleForm()
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():# https://stackoverflow.com/questions/68389156/django-how-to-set-user-create-article-is-by-user-is-logon
+            article = form.save(commit=False)
+            article.author = request.user.profile
+            article.save()
+            return redirect('wiki:article-list')
+    
+    ctx = {'form' : form}
+    return render(request, 'wiki_add.html', ctx)
+
+def article_update(request, pk):
+    '''Article update view for wiki articles'''
+    article = Article.objects.get(pk=pk)
+    form = ArticleForm()
+    if request.method == 'POST':
+        form = ArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.save()
+            return redirect('wiki:article-detail', article.pk)
+    
+    ctx = {
+        'form' : form,
+        'article' : article
+    }
+    return render(request, 'wiki_update.html', ctx)
