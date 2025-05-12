842b822f10eaf0b8d8815178915ae5ef154707ff
Matthew <lmbalisacan@gmail.com>
2025-05-12 23:53:17 +0800

    Done(ish) with blog app
    I think I've got all the specs down, only problem is I can't seem to get the header images to display

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,9 +1,10 @@
 from . import views 
 from django.urls import path
+from .views import (blog_list, blog_detail, ArticleCreateView, ArticleUpdateView)
 
 urlpatterns = [
-    path('articles/', views.blog_list, name='article_list'),
-    path('article/<int:article_id>/', views.blog_detail, name='article_detail'),
+    path('articles/', blog_list, name='article_list'),
+    path('article/<int:article_id>/', blog_detail, name='article_detail'), 
+    path('article/add/', ArticleCreateView.as_view(), name='article_create'),
+    path('article/<int:pk>/edit/', ArticleUpdateView.as_view(), name='article_update'),
 ]
-
-app_name = 'blog'
--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,13 +1,75 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Article, ArticleCategory
+from .forms import CommentForm, ArticleForm
+from django.db.models import Prefetch
+from django.utils.decorators import method_decorator
+from user_management.models import Profile
 
 # Create your views here.
 
 def blog_list(request):
-    articles = Article.objects.all()  
-    return render(request, 'blog/blog_list.html', {'articles': articles})
+    articles = Article.objects.all().order_by('-created_on')
+    user_articles = Article.objects.none()
+    
+    if request.user.is_authenticated:
+        user_articles = articles.filter(author__user=request.user)
+        articles = articles.exclude(author__user=request.user)
+    
+    categories = ArticleCategory.objects.all().prefetch_related(Prefetch('article_set', queryset=articles))
+    
+    context = {
+        'user_articles': user_articles,
+        'categories': categories,
+        'articles': articles,
+    }
+    return render(request, 'blog/blog_list.html', context)
 
+@login_required
 def blog_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
-    return render(request, 'blog/blog_detail.html', {'article': article})
+    comments = article.comment_set.all().order_by('created_on')
+    related_articles = Article.objects.filter(author=article.author).exclude(id=article.id)[:2]
+    
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user.profile
+            comment.article = article
+            comment.save()
+    else:
+        form = CommentForm()
+    
+    context = {
+        'article': article,
+        'related_articles': related_articles,
+        'comments': comments,
+        'form': form,
+    }
+    return render(request, 'blog/blog_detail.html', context)
+
+@method_decorator(login_required, name='dispatch')
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'blog/article_form.html'
+    success_url = reverse_lazy('article_list')
+    
+    def form_valid(self, form):
+        profile, created = Profile.objects.get_or_create(user=self.request.user)
+        form.instance.author = profile
+        return super().form_valid(form)
+
+@method_decorator(login_required, name='dispatch')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleForm
+    template_name = 'blog/article_form.html'
+    success_url = reverse_lazy('article_list')
+
+    def get_queryset(self):
+        return super().get_queryset().filter(author__user=self.request.user)
