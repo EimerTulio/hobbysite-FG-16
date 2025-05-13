2567db9312d5797b6670cb3d57487b0d27e1bc53
Matthew <lmbalisacan@gmail.com>
2025-05-13 15:20:21 +0800

    Fixed the stuff based on PR #17
    - added app_name and changed all urls accordingly
    - changed str(self.id) to int(self.id) in models.py
    - reworked views in a way that doesn't use prefetch
    - removed @login_required for blog_detail and the [:2]
    - moved Comment Form to appear before existing comments

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -5,34 +5,32 @@ from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView
 from .models import Article, ArticleCategory
 from .forms import CommentForm, ArticleForm
-from django.db.models import Prefetch
 from django.utils.decorators import method_decorator
 from user_management.models import Profile
 
 # Create your views here.
 
 def blog_list(request):
-    articles = Article.objects.all().order_by('-created_on')
-    user_articles = Article.objects.none()
+    all_articles = Article.objects.all().order_by('-created_on')
+    user_articles = Article.objects.none() 
     
     if request.user.is_authenticated:
-        user_articles = articles.filter(author__user=request.user)
-        articles = articles.exclude(author__user=request.user)
+        user_articles = all_articles.filter(author__user=request.user)
+        all_articles = all_articles.exclude(author__user=request.user)
     
-    categories = ArticleCategory.objects.all().prefetch_related(Prefetch('article_set', queryset=articles))
+    categories = ArticleCategory.objects.all().order_by('name')
     
     context = {
-        'user_articles': user_articles,
-        'categories': categories,
-        'articles': articles,
+        'user_articles': user_articles,  
+        'categories': categories,        
+        'all_articles': all_articles,   
     }
     return render(request, 'blog/blog_list.html', context)
 
-@login_required
 def blog_detail(request, article_id):
     article = get_object_or_404(Article, id=article_id)
     comments = article.comment_set.all().order_by('created_on')
-    related_articles = Article.objects.filter(author=article.author).exclude(id=article.id)[:2]
+    related_articles = Article.objects.filter(author=article.author).exclude(id=article.id)
     
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -57,7 +55,7 @@ class ArticleCreateView(CreateView):
     model = Article
     form_class = ArticleForm
     template_name = 'blog/article_form.html'
-    success_url = reverse_lazy('article_list')
+    success_url = reverse_lazy('blog:article_list')
     
     def form_valid(self, form):
         profile, created = Profile.objects.get_or_create(user=self.request.user)
@@ -69,7 +67,7 @@ class ArticleUpdateView(UpdateView):
     model = Article
     form_class = ArticleForm
     template_name = 'blog/article_form.html'
-    success_url = reverse_lazy('article_list')
+    success_url = reverse_lazy('blog:article_list')
 
     def get_queryset(self):
         return super().get_queryset().filter(author__user=self.request.user)
