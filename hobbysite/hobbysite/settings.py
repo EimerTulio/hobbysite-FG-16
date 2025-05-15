31b25bb32ace6fb1cb9fd0eb2a3b46d9eb44c1c9
Alba Martin <martin.alba@student.ateneo.edu>
2025-05-15 19:59:04 +0800

    debug is now set to false

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '^)53ed!i-%=7zl4$&4#f))3mysqhp26-ylbc=p5q4p8gr)75rn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
