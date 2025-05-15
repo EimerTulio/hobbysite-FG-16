f346aa8e3fba2f310527b7a938c664a45c32d118
Alba Martin <martin.alba@student.ateneo.edu>
2025-05-15 20:42:57 +0800

    secret key cooked

--- hobbysite/hobbysite/settings.py
+++ hobbysite/hobbysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^)53ed!i-%=7zl4$&4#f))3mysqhp26-ylbc=p5q4p8gr)75rn'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
